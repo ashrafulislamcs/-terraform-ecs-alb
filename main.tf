6fd21e1bc52b3d01e91fdbd9bc13a8e4a56b98de
Brodey Newman <brodeynewman@gmail.com>
2021-01-04 17:51:17 -0500

    fixing load balancing

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "label" {
   source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=master"
   namespace  = "everlook"
   stage      = "dev"
-  name       = "node-ecr"
+  name       = "node-ecs"
   attributes = ["public"]
   delimiter  = "-"
 }
@@ -28,7 +28,7 @@ data "aws_iam_role" "ecs_task_execution_role" {
 }
 
 resource "aws_ecs_task_definition" "node" {
-  family                   = "node-ecr"
+  family                   = "node-ecs"
   network_mode             = var.network_mode
   requires_compatibilities = [var.ecs_launch_type]
   cpu                      = var.task_cpu
@@ -46,7 +46,7 @@ resource "aws_ecs_task_definition" "node" {
     "essential": true,
     "image": "${module.ecr.repository_url}:latest",
     "memory": ${var.task_memory},
-    "name": "node-ecr",
+    "name": "node-ecs",
     "portMappings": [
       {
         "containerPort": 8080,
@@ -68,20 +68,62 @@ resource "aws_default_vpc" "default" {
   }
 }
 
-resource "aws_default_subnet" "default_az1" {
-  availability_zone = "us-east-1a"
+resource "aws_default_subnet" "az1" {
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "Default subnet for us-east-1a"
   }
 }
 
-resource "aws_lb_target_group" "node" {
-  name        = "node-ecr-lb-tg"
+resource "aws_default_subnet" "az2" {
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Default subnet for us-east-1b"
+  }
+}
+
+resource "aws_default_security_group" "lb_sg" {
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+}
+
+resource "aws_lb" "ecs" {
+  name               = "ecs-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_default_security_group.lb_sg.id]
+  subnets            = [aws_default_subnet.az1.id, aws_default_subnet.az2.id]
+}
+
+resource "aws_lb_target_group" "ecs" {
+  name        = "node-ecs-lb-tg"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_default_vpc.default.id
   target_type = "ip"
+
+  depends_on = [aws_lb.ecs]
+}
+
+resource "aws_lb_listener" "default" {
+  load_balancer_arn = aws_lb.ecs.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs.arn
+  }
 }
 
 resource "aws_ecs_service" "node" {
@@ -92,14 +134,15 @@ resource "aws_ecs_service" "node" {
   deployment_minimum_healthy_percent = var.deployment_minimum_healthy_percent 
   desired_count                      = 2
   launch_type                        = var.ecs_launch_type
+  depends_on                         = [aws_lb_target_group.ecs]
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.node.arn
-    container_name   = "node-ecr"
+    target_group_arn = aws_lb_target_group.ecs.arn
+    container_name   = "node-ecs"
     container_port   = 8080
   }
 
   network_configuration {
-    subnets = [aws_default_subnet.default_az1.id]
+    subnets = [aws_default_subnet.az1.id]
   }
 }
