8dd0f190974efdc6fb80cc7b8bc41c2db7111151
Brodey Newman <brodeynewman@gmail.com>
2021-01-17 14:59:07 -0500

    fixing vars

--- variables.tf
+++ variables.tf
@@ -28,19 +28,19 @@ variable ecs_launch_type {
   description = "ECS launch type"
 }
 
-variable "network_mode" {
+variable network_mode {
   type        = string
   default     = "awsvpc"
   description = "The network mode to use for the task. This is required to be `awsvpc` for `FARGATE` `launch_type`"
 }
 
-variable "task_cpu" {
+variable task_cpu {
   type        = number
   default     = 256
   description = "The number of CPU units used by the task. If using `FARGATE` launch type `task_cpu` must match supported memory values (https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#task_size)"
 }
 
-variable "task_memory" {
+variable task_memory {
   type        = number
   default     = 512
   description = "The amount of memory (in MiB) used by the task. If using Fargate launch type `task_memory` must match supported cpu value (https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#task_size)"
