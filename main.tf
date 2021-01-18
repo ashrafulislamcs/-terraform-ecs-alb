d10e8c2f980a6fd6fc9790a8458edca4ff283d2d
Brodey Newman <brodeynewman@gmail.com>
2021-01-17 20:30:05 -0500

    force ecs service deployment

--- main.tf
+++ main.tf
@@ -185,7 +185,7 @@ resource "aws_security_group" "alb_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.main.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
