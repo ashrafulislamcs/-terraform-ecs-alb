c84279db5d4dd647f13547a1e1e22b4c4e8ff94d
Brodey Newman <brodeynewman@gmail.com>
2021-05-25 12:27:45 -0400

    fixing listener

--- main.tf
+++ main.tf
@@ -278,17 +278,6 @@ resource "aws_lb_listener" "alb_http" {
   }
 }
 
-resource "aws_lb_listener" "alb_http" {
-  load_balancer_arn = aws_lb.alb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.alb.arn
-  }
-}
-
 resource "aws_ecs_service" "node" {
   name                               = module.label.name
   cluster                            = aws_ecs_cluster.default.id
